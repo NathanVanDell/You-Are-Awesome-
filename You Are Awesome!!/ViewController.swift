fdf80551a7934b3f1e2efeb923bedf41645b39f3
Nathan Van Dell <nvdski@gmail.com>
2020-09-16 15:26:51 -0700

    2.15

--- You Are Awesome!!/ViewController.swift
+++ You Are Awesome!!/ViewController.swift
@@ -6,32 +6,55 @@
 //  Copyright © 2020 Nathan Van Dell. All rights reserved.
 //
 
+
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
+    var imageNumber = -1
+    var messageNumber = -1
+    let totalNumberOfImages = 9
+    var audioPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         messageLabel.text = ""
     }
-    
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        let awesomeMessage = "You Are Awesome!"
-        let greatMessage = "You Are Great!"
-        let bombMessage = "You Are Da Bomb!"
-        if messageLabel.text == awesomeMessage {
-            messageLabel.text = greatMessage
-            imageView.image = UIImage(named: "image1")
-        } else if messageLabel.text == greatMessage{
-            messageLabel.text = bombMessage
-            imageView.image = UIImage(named: "image2")
-        }else {
-            messageLabel.text = awesomeMessage
-            imageView.image = UIImage(named: "image0")
-        }
+        let messages = ["You Are Awesome",
+                        "You Are Great",
+                        "You Are Fantastic!",
+                        "When the Genius Bar Needs Help, They Call You",
+                        "Fabulous? That's You!",
+                        "You've Got The Design Skills of Jony Ive"]
         
+        var newMessageNumber: Int
+        repeat {
+            newMessageNumber = Int.random(in: 0...messages.count-1)
+        } while messageNumber == newMessageNumber
+        messageNumber = newMessageNumber
+        messageLabel.text = messages[messageNumber]
+        
+        var newImageNumber: Int
+        repeat {
+            newImageNumber = Int.random(in: 0...totalNumberOfImages)
+        } while imageNumber == newImageNumber
+        imageNumber = newImageNumber
+        imageView.image = UIImage(named: "image\(imageNumber)")
+        if let sound = NSDataAsset(name: "sound0") {
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("🤬 ERROR: \(error.localizedDescription) Could not initialize AVAudioPlayer object")
+            }
+            
+        } else {
+            print("🤬 ERROR: Could not read data from file sound0")
+        }
     }
     
 }
